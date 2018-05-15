872ae2376e1f204d4d0d33b599ddb06581d11e7c
Jefferson <jeffersonhcarvalho@gmail.com>
2018-05-15 12:54:05 -0300

    Still have an error, even though the tests pass(lol)

--- src/test/cat.ts
+++ src/test/cat.ts
@@ -24,77 +24,77 @@ class CatsTest {
     }
 
     @test("/GET cat - Should get all cats in the db")
-    public getAll(done: Function) {
+    public getAll() {
         chai.request(server.instance)
         .get('/cat')
         .set('Content-Type', 'application/json')
-        .end((err, res) => {
+        .then(res => {
             assert.equal(res.status, 200, 'The http code is wrong');
             assert.typeOf(res.body.data, 'array', `The response is ill-formed: ${res.body}`);
             assert.lengthOf(res.body.data, 0, 'There are elements in the response.');
-            done();
-        });
+        })
+        .catch(err => {throw err});
     }
 
     @test("/POST - Should create a cat")
-    public create(done: Function) {
+    public create() {
         chai.request(server.instance)
         .post('/cat')
         .send(mockCat)
         .set('Content-Type', 'application/json')
-        .end((err, res) => {
+        .then(res => {
             assert.equal(res.status, 201, 'The http code is wrong');
             assert.equal(res.body.data.name, mockCat.name, 'The name is wrong');
             assert.equal(res.body.data.color, mockCat.color, 'The color is wrong');
             assert.equal(res.body.data.age, mockCat.age, 'The age is wrong');
             assert.typeOf(res.body.errors, 'null', `${res.body.errors}`);
             CatsTest.catId = res.body.data._id;
-            done();
-        });
+        })
+        .catch(err => {throw err});
     }
 
     @test("/GET - Should get the cat created in POST")
-    public getByName(done: Function) {
+    public getByName() {
         chai.request(server.instance)
         .get(`/cat/${mockCat.name}`)
         .set('Content-Type', 'application/json')
-        .end((err, res) => {
+        .then(res => {
             assert.equal(res.status, 200, 'The http code is wrong');
             assert.equal(res.body.data.name, mockCat.name, 'The name is wrong');
             assert.equal(res.body.data.color, mockCat.color, 'The color is wrong');
             assert.equal(res.body.data.age, mockCat.age, 'The age is wrong');
             assert.typeOf(res.body.errors, 'null', `${res.body.errors}`);
-            done();
-        });
+        })
+        .catch(err => {throw err});
     }
 
     @test("/PUT - Should update the cat")
-    public update(done: Function) {
+    public update() {
         chai.request(server.instance)
         .put(`/cat/${CatsTest.catId}`)
         .set('Content-Type', 'application/json')
         .send({name: "Bolota"})
-        .end((err, res) => {
+        .then(res => {
             assert.equal(res.status, 200, 'The http code is wrong');
             assert.equal(res.body.data.name, 'Bolota', 'The name is wrong');
             assert.equal(res.body.data.color, mockCat.color, 'The color is wrong');
             assert.equal(res.body.data.age, mockCat.age, 'The age is wrong');
             assert.typeOf(res.body.errors, 'null', `${res.body.errors}`);
-            done();
-        });
+        })
+        .catch(err => {throw err});
     }
 
     @test("/DELETE - Should delete the cat")
-    public remove(done: Function) {
+    public remove() {
         chai.request(server.instance)
         .del(`cat/${CatsTest.catId}`)
         .set('Content-Type', 'application/json')
-        .end((err, res) => {
+        .then(res => {
             assert.equal(res.status, 204, 'The http code is wrong');
             assert.typeOf(res.body.data, 'null', `${res.body.data}`);
             assert.typeOf(res.body.errors, 'null', `${res.body.errors}`);
-            done();
-        });
+        })
+        .catch(err => {throw err});
     }
 
     public static after() {
