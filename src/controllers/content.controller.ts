c8875f8616d729c6b8d6363da878d9934e890399
Jefferson Carvalho <jeffersonhcarvalho@gmail.com>
2019-09-10 20:31:15 -0300

    feat(content): protect content endpoints with auth

--- src/controllers/content.controller.ts
+++ src/controllers/content.controller.ts
@@ -6,59 +6,82 @@ import { IUser } from '../db/user.db'
 
 export class ContentController {
   public static getAll(req: Request, res: Response) {
-    passport.authenticate('jwt', { session: false }, (_: Error, user: IUser) => {
-      if (user) {
-        ContentModel.find({ createdBy: user._id }, (err: Error, docs: IContent[]) => {
-          res.status(200).json({ data: docs, errors: err })
-        })
+    passport.authenticate('jwt', { session: false }, async (err: Error, user: IUser) => {
+      let data: IContent[] | null = null
+      let errors: object[] = err ? [err] : []
+
+      res.statusCode = err || !user ? 401 : 200
+
+      try {
+        data = await ContentModel.find({ createdBy: user._id })
+      } catch (err) {
+        errors.push(err)
       }
+
+      res.json({ data, errors })
     })(req, res)
   }
 
   public static getById(req: Request, res: Response) {
-    const contentId = req.params.id
+    passport.authenticate('jwt', { session: false }, async (err: Error, user: IUser) => {
+      const contentId = req.params.id
+      let data: IContent | null = null
+      let errors: object[] = err ? [err] : []
 
-    passport.authenticate('jwt', { session: false }, (_: Error, user: IUser) => {
-      if (user) {
-        ContentModel.findOne({ createdBy: user._id, _id: contentId }, (err: Error, content: IContent) => {
-          res.statusCode = err || content == null ? 404 : 200
-          res.json({ data: content, errors: err })
-        })
+      res.statusCode = err || !user ? 401 : 200
+
+      try {
+        data = await ContentModel.findOne({ createdBy: user._id, _id: contentId })
+        if (!data) {
+          res.statusCode = 404
+        }
+      } catch (err) {
+        errors.push(err)
       }
+
+      res.json({ data, errors })
     })(req, res)
   }
 
   public static create(req: Request, res: Response) {
-    passport.authenticate('jwt', { session: false }, (_: Error, user: IUser) => {
+    passport.authenticate('jwt', { session: false }, (authErr: Error, user: IUser) => {
       const content = new ContentModel({ ...req.body, createdBy: user._id })
+      let errors: object[] = authErr ? [authErr] : []
 
       content.save((err: Error, doc: IContent) => {
+        if (err) {
+          errors.push(err)
+        }
         res.statusCode = err ? 406 : 201
         if (!err) {
           res.location(`/content/${doc._id}`)
         }
-        res.json({ data: doc, errors: err })
+        res.json({ data: doc, errors })
       })
     })(req, res)
   }
 
   public static update(req: Request, res: Response) {
-    passport.authenticate('jwt', { session: false }, (_: Error, user: IUser) => {
+    passport.authenticate('jwt', { session: false }, (authErr: Error, user: IUser) => {
       const query = {
         createdBy: user._id,
         _id: req.params.id,
       }
+      let errors: object[] = authErr ? [authErr] : []
 
       ContentModel.findOneAndUpdate(query, req.body, { new: true }, (err: Error, content: IContent | null) => {
         res.statusCode = err || content == null ? 406 : 200
-        res.json({ data: content, errors: err })
+        if (err) {
+          errors.push(err)
+        }
+        res.json({ data: content, errors })
       })
     })(req, res)
   }
 
   public static async remove(req: Request, res: Response) {
-    passport.authenticate('jwt', { session: false }, async (_: Error, user: IUser) => {
-      let errors: object | null = null
+    passport.authenticate('jwt', { session: false }, async (err: Error, user: IUser) => {
+      let errors: object[] = err ? [err] : []
       res.statusCode = 406
 
       try {
@@ -71,7 +94,7 @@ export class ContentController {
           res.statusCode = 404
         }
       } catch (err) {
-        errors = err
+        errors.push(err)
       }
       if (res.statusCode === 204) {
         res.json()
